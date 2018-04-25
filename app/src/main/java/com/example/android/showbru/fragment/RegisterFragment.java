29f81726fd8c9f55c6ba6f21899d3d4ca28049fb
premmy <csbongga@gmail.com>
2018-04-25 17:10:16 +0700

    Finish 250461

--- app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
@@ -4,13 +4,46 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.showbru.MainActivity;
 import com.example.android.showbru.R;
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    } //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+//        Setup Nevigation Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
