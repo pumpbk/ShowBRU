b1d370c74cdceb4fbe8a7ea96d82200964ed3152
premmy <csbongga@gmail.com>
2018-04-26 12:03:33 +0700

    Finish 250461

--- app/src/main/java/com/example/android/showbru/utility/AddNewUserToServer.java
+++ app/src/main/java/com/example/android/showbru/utility/AddNewUserToServer.java
@@ -1,8 +1,14 @@
 package com.example.android.showbru.utility;
 
+import android.content.Context;
+
 public class AddNewUserToServer {
 
+    private Context context;
 
+    public AddNewUserToServer(Context context) {
+        this.context = context ;
+    }
 
 
 }
