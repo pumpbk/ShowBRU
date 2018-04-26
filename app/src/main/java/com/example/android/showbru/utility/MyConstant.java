6f42994f93988636f528f6900006459b578c18b9
premmy <csbongga@gmail.com>
2018-04-26 11:58:59 +0700

    Finish 250461

---
+++ app/src/main/java/com/example/android/showbru/utility/MyConstant.java
@@ -0,0 +1,10 @@
+package com.example.android.showbru.utility;
+
+public class MyConstant {
+
+    private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
