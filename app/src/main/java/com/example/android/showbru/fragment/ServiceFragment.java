9e0d48b71d9e4ee71383cfa1ba7c7cc4f96dbaf2
premmy <csbongga@gmail.com>
2018-04-26 17:42:47 +0700

    260461 Finish

--- app/src/main/java/com/example/android/showbru/fragment/ServiceFragment.java
+++ app/src/main/java/com/example/android/showbru/fragment/ServiceFragment.java
@@ -7,10 +7,66 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.android.showbru.R;
+import com.example.android.showbru.utility.FoodAdapter;
+import com.example.android.showbru.utility.GetAllData;
+import com.example.android.showbru.utility.MyConstant;
 
-public class ServiceFragment extends Fragment{
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ServiceFragment extends Fragment {
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create ListView
+        createListView();
+
+
+    } //Main Method
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.listViewFood);
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlGetAllFood());
+
+            String jsonString = getAllData.get();
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] foodStrings = new String[jsonArray.length()];
+            String[] priceStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] imageStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length(); i+=1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                foodStrings[i] = jsonObject.getString("NameFood");
+                priceStrings[i] = jsonObject.getString("Price");
+                detailStrings[i] = jsonObject.getString("Detail");
+                imageStrings[i] = jsonObject.getString("ImagePath");
+
+            }
+
+            FoodAdapter foodAdapter = new FoodAdapter(getActivity(),
+                    imageStrings, foodStrings, priceStrings, detailStrings);
+            listView.setAdapter(foodAdapter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Nullable
     @Override
--- app/src/main/java/com/example/android/showbru/utility/MyConstant.java
+++ app/src/main/java/com/example/android/showbru/utility/MyConstant.java
@@ -4,6 +4,11 @@ public class MyConstant {
 
     private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
     private String urlGetUser = "http://androidthai.in.th/bru/getAllUser.php";
+    private String urlGetAllFood = "http://androidthai.in.th/bru/getAllFood.php";
+
+    public String getUrlGetAllFood() {
+        return urlGetAllFood;
+    }
 
     public String getUrlGetUser() {
         return urlGetUser;
