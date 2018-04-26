33d4c3746e3d1046b1f01150b70bebe561e99f24
premmy <csbongga@gmail.com>
2018-04-26 11:49:24 +0700

    Finish 250461

--- app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
@@ -6,11 +6,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.android.showbru.MainActivity;
 import com.example.android.showbru.R;
+import com.example.android.showbru.utility.MyAlert;
 
 public class RegisterFragment extends Fragment {
 
@@ -24,9 +29,58 @@ public class RegisterFragment extends Fragment {
 
     } //Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            uploadValueToServer();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void uploadValueToServer() {
+
+//        get_value_from_EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//        Change Date_Type From EditText to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+//        Check Space
+        if (nameString.isEmpty()|| userString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+
+        } else {
+//            No Space
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
     private void createToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
 //        Setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -42,6 +96,9 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
+
     }
 
     @Nullable
